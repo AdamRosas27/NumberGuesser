3338425e0910fd11890e5215df783191a5320e28
Adam Rosas <adamrosas27@gmail.com>
2022-10-02 21:54:28 -0700

    Add comments
    Add comments with overall layout and structure of the project (ie. figure out what is needed for the project)

---
+++ guessNumber.py
@@ -0,0 +1,9 @@
+# Import Random Module that will be used for using the random function
+
+# Define number guesser method that accepts no parameters
+# Print out a welcome statement and lower and upper bounds of the guess
+# Initlize a variable that is set equal to the random number that the user has to try to guess
+# Asks the user to enter a number and initilize a variable that is equal to the user input
+# If guess is too high, ask them to enter another guess
+# If guess is too low, ask them to enter another guess
+# If the user input and the random number are the same then print a statement saying they got it right
