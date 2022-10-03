4edca20cda34c79aaae5e76a44556d2720a22746
Adam Rosas <adamrosas27@gmail.com>
2022-10-02 22:18:17 -0700

    Initilize answer variable that is the target number the user has to guess

--- guessNumber.py
+++ guessNumber.py
@@ -1,13 +1,14 @@
 # Import Random Module that will be used for using the random function
-from random import random as randy
+from random import Random, random
 
 # Define number guesser method that accepts no parameters
 
 
 def guessTheNum():
-    print('Welcome to Guess The Number! \nI am thinking of a number between 1 and 30. \nCan you guess what it is?')
     # Print out a welcome statement and lower and upper bounds of the guess
+    print('Welcome to Guess The Number! \nI am thinking of a number between 1 and 30. \nCan you guess what it is?')
     # Initlize a variable that is set equal to the random number that the user has to try to guess
+    answer = Random.randint(1, 30)
     # Asks the user to enter a number and initilize a variable that is equal to the user input
     # If guess is too high, ask them to enter another guess
     # If guess is too low, ask them to enter another guess
