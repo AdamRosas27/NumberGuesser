60a685c9052b5496da1c10f98a7f473341230cd1
Adam Rosas <adamrosas27@gmail.com>
2022-10-02 22:14:13 -0700

    Fix bug in welcome message

--- guessNumber.py
+++ guessNumber.py
@@ -5,10 +5,13 @@ from random import random as randy
 
 
 def guessTheNum():
-    print('Welcome to Guess The Number! \nI am thinking of a number between 1 and 30. Can you guess what it is?')
+    print('Welcome to Guess The Number! \nI am thinking of a number between 1 and 30. \nCan you guess what it is?')
     # Print out a welcome statement and lower and upper bounds of the guess
     # Initlize a variable that is set equal to the random number that the user has to try to guess
     # Asks the user to enter a number and initilize a variable that is equal to the user input
     # If guess is too high, ask them to enter another guess
     # If guess is too low, ask them to enter another guess
     # If the user input and the random number are the same then print a statement saying they got it right
+
+
+guessTheNum()
