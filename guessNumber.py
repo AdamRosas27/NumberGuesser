e54872eac5d2ae683e63bc2cf0f7c5e74b80b7ab
Adam Rosas <adamrosas27@gmail.com>
2022-10-02 22:20:10 -0700

    Add while loop that keeps asking for input until user enters valid input

--- guessNumber.py
+++ guessNumber.py
@@ -10,9 +10,11 @@ def guessTheNum():
     # Initlize a variable that is set equal to the random number that the user has to try to guess
     answer = Random.randint(1, 30)
     # Asks the user to enter a number and initilize a variable that is equal to the user input
-    # If guess is too high, ask them to enter another guess
-    # If guess is too low, ask them to enter another guess
-    # If the user input and the random number are the same then print a statement saying they got it right
+    while True:
+
+        # If guess is too high, ask them to enter another guess
+        # If guess is too low, ask them to enter another guess
+        # If the user input and the random number are the same then print a statement saying they got it right
 
 
 guessTheNum()
